5549c1dbd9b81e6571500d533e822ac9035b0134
shankar dahal <86343933+net13s@users.noreply.github.com>
2022-04-02 20:40:19 +0545

    Add files via upload

---
+++ facebook_automate.py
@@ -0,0 +1,11 @@
+
+from urllib.request import DataHandler
+import pyautogui
+import time
+while True:
+
+    time.sleep(5)
+    pyautogui.typewrite("This is a shankar dahal")
+
+    pyautogui.press('enter')
+
